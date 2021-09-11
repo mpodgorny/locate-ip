2c36b4cbb7695c5d419bfeeb829b5f27240885c2
Michał Podgórny <m.podgorny@bonasoft.pl>
2021-09-11 20:04:22 +0200

    added: current searched; skeleton in current local

--- src/components/TextRow.tsx
+++ src/components/TextRow.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import { Typography, Grid } from '@material-ui/core';
+import { Typography, Grid, Box } from '@material-ui/core';
 
 type TextRowProps = { title: string; value: string };
 
 const TextRow: React.FC<TextRowProps> = ({ title, value }) => {
+  const parseKey = (key: string): string => key.replace('_', ' ');
+
   return (
-    <Grid container>
-      <Grid item xs={5}>
-        <Typography>{title}</Typography>
+    <Box p={1}>
+      <Grid container>
+        <Grid item xs={5}>
+          <Typography>{parseKey(title)}</Typography>
+        </Grid>
+        <Grid item xs={7}>
+          <Typography>{value}</Typography>
+        </Grid>
       </Grid>
-      <Grid item xs={7}>
-        <Typography>{value}</Typography>
-      </Grid>
-    </Grid>
+    </Box>
   );
 };
 
