3ca4d7ea5ea7265651078015ea90dfa8a68451bf
Michał Podgórny <m.podgorny@bonasoft.pl>
2021-09-11 19:47:44 +0200

    custom card margins + improvments

--- src/components/CustomCard.tsx
+++ src/components/CustomCard.tsx
@@ -1,17 +1,44 @@
 import React from 'react';
-import { Card, CardHeader, Box } from '@material-ui/core';
+import { Card, CardHeader, Box, makeStyles } from '@material-ui/core';
 
-type CustomCardProps = { title?: string; children?: JSX.Element };
+type CustomCardProps = {
+  title?: string;
+  disableMargin?: boolean;
+  children?: JSX.Element;
+  subheader?: string;
+};
 
-const CustomCard: React.FC<CustomCardProps> = ({ title = '', children }) => {
+const useStyles = makeStyles(() => ({
+  root: { height: '100%', position: 'relative' },
+  header: {
+    padding: '4px 16px',
+  },
+  content: {
+    height: '100%',
+  },
+}));
+
+const CustomCard: React.FC<CustomCardProps> = ({
+  title = '',
+  subheader = '',
+  disableMargin = false,
+  children,
+}) => {
+  const classes = useStyles();
   return (
-    <Box m={{ xs: 1 }}>
-      <Card raised>
-        <CardHeader
-          title={title}
-          titleTypographyProps={{ variant: 'subtitle2' }}
-        />
-        {children}
+    <Box p={disableMargin ? 0 : 1} className={classes.root}>
+      <Card raised className={classes.root}>
+        {title && (
+          <CardHeader
+            className={classes.header}
+            title={title}
+            titleTypographyProps={{ variant: 'subtitle2' }}
+            subheader={subheader}
+          />
+        )}
+        <Box p={1} height="100%" width="100%">
+          {children}
+        </Box>
       </Card>
     </Box>
   );
