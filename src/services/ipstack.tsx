b3a29fa89384eac48d188cda30b39328337cad75
Michał Podgórny <m.podgorny@bonasoft.pl>
2021-09-12 09:57:53 +0200

    added proper types

--- src/services/ipstack.tsx
+++ src/services/ipstack.tsx
@@ -1,4 +1,10 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
+
+export type getDataType = {
+  data: Record<string, unknown>;
+  latitude: number;
+  longitude: number;
+};
 
 const api: AxiosInstance = axios.create({
   baseURL: 'http://api.ipstack.com/',
@@ -7,11 +13,11 @@ const api: AxiosInstance = axios.create({
   },
 });
 
-const getCurrentLocation = (): Promise<Record<string, any>> => {
+const getCurrentLocation = (): Promise<AxiosResponse<getDataType>> => {
   return api.get('check/');
 };
 
-const getSearch = (query: string): Promise<Record<string, any>> => {
+const getSearch = (query: string): Promise<AxiosResponse<getDataType>> => {
   return api.get(query, { params: { query } });
 };
 
